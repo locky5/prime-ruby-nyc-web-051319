6a5d38ccf36d105cc3359a22c7463b667bf5bf79
Kevin Dai <daikevin19@yahoo.com>
2019-04-29 18:26:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(int)
-  if int < 0
-    return false 
-  end
   for d in 2..(int-1)
     if (int % d) == 0
       return false
     end
+  end
+  if int < 0
+    return false 
   end
   return true
 end
