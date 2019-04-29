4f0fd41cd32e38e3de8ea378dad7c422c3a0693f
Kevin Dai <daikevin19@yahoo.com>
2019-04-29 18:27:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+  for d in 2..(int-1)
+    if (int % d) == 0 #checks remainder for every value up to -1 of actual int
+      return false
+    end
+  end
+  if int <= 1
+    return false 
+  end
+  return true
+end
